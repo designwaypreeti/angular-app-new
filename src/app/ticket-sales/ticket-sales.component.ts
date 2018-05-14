e7322b6ffc9351b5f61aa7b58cbbf60a16970b57
srikar <vasta.srikar@gmail.com>
2018-05-14 11:02:20 +0530

    fixed param issue

--- src/app/ticket-sales/ticket-sales.component.ts
+++ src/app/ticket-sales/ticket-sales.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-ticket-sales',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ticket-sales.component.less']
 })
 export class TicketSalesComponent implements OnInit {
-
+  @Input() eId 
   constructor() { }
 
   ngOnInit() {
