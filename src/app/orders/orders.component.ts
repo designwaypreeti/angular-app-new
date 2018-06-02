99af3d3d1810d3dd700eb25748a879db7c03495a
srikar <srikar@niiu.de>
2018-06-03 01:02:03 +0530

    ...

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -9,7 +9,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class OrdersComponent implements OnInit {
   orders
-  uId
+  uId 
   user
   constructor( private mainService: MainService, private route: ActivatedRoute) {
     this.user = JSON.parse(localStorage.getItem('user'))
